defc4b87a1fe862fb63afb3a835769f96f055eea
Fernando-Affonso <fernando.affonso@connectparts.com.br>
2021-11-03 15:24:35 -0300

    Começada as alterações no Login pra ele ficar definitivo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify';
 import router from './router';
 import api from './service';
 import store from './store';
+import rules from './plugins/rules';
 import VueAxios from "vue-axios";
 // import VueCurrencyFilter from "vue-currency-filter";
 // import "./plugins/vuetify-money.js";
@@ -39,6 +40,7 @@ new Vue({
   vuetify,
   router,
   store,
+  rules,
   render: h => h(App)
 }).$mount('#app')
 
---
+++ src/plugins/rules.js
@@ -0,0 +1,10 @@
+const rules = {
+    required: value => !!value || 'Required.',
+    counter: value => value.length <= 20 || 'Max 20 characters',
+    email: value => {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return pattern.test(value) || 'Invalid e-mail.'
+    },
+}
+
+export default rules;
